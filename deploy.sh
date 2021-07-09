3d42cecf5d8e8e0966f34f29b86be8a9667ce489
Michal Atlas <michal.z.atlas@gmail.com>
2021-07-09 04:28:16 +0200

    Added deploy script

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -euox pipefail
+
+git worktree add /tmp/gh-pages gh-pages
+emacs DawnStorm.org --batch -f 'org-html-export-to-html' --kill
+mv DawnStorm.html /tmp/gh-pages/
+cd /tmp/gh-pages
+git add .
+git commit -m "$(date -Ins)"
+git push
+cd -
+git worktree remove /tmp/gh-pages
