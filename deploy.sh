7e32a5ef574c6c47d16a86019910b5411e042426
Michal Atlas <michal.z.atlas@gmail.com>
2021-07-09 04:34:26 +0200

    Removed unneded files

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ set -euox pipefail
 
 git worktree add /tmp/gh-pages gh-pages
 emacs DawnStorm.org --batch -f 'org-html-export-to-html' --kill
-mv DawnStorm.html /tmp/gh-pages/
+mv DawnStorm.html /tmp/gh-pages/index.html
 cd /tmp/gh-pages
 git add .
 git commit -m "$(date -Ins)"
--- index.md
+++
@@ -1,5 +0,0 @@
----
-layout: home
----
-
-<meta http-equiv="refresh" content="0; url = https://dawnstorm.michal-atlas.co/system"/>
